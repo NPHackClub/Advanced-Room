67d098287340ab15063029d395dcd859ae317556
TheDe-bugger <jaimildalwadi2003@gmail.com>
2021-02-11 15:03:15 -0500

    removed node modules

--- Jaimil/rn-starter/src/screens/HomeScreen.js
+++ Jaimil/rn-starter/src/screens/HomeScreen.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, Image, View } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hello Sam</Text>;
+  return (
+  <View>
+      <Text style={styles.text}>Hello Sam</Text>
+      <Image style = {styles.image} source = {require("./bg-01.png")}/>
+  </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 100
+  },
+  image: {
+    width: 500,
+    height: 500,
   }
 });
 
